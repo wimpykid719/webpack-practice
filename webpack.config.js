3eabfafa14adf96c19304823fcd224dfcd6cbcea
“wataru” <ltomu.web@gmail.com>
2021-08-11 09:22:01 +0900

    fix webpack.config.js can support the path for another os enviroment

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
+const path = require('path')
+
 module.exports = {
   // 作業ディレクトリがある場合はentryの書き方が変わる
   context: `${__dirname}/src`,
-  entry: './index.js',
+  // entry: './index.js',
+  // joinを使用するとOSの環境に合わせてパスをいい感じに設定してくれる。
+  entry: path.join(__dirname, '/src', 'index.js'),
 
   // defaultの設定つまりあってもなくても一緒
   // ファイルの起点を設定する。
@@ -9,7 +13,8 @@ module.exports = {
 
   // ここは絶対パスで指定する必要がある。
   output: {
-    path: `${__dirname}/dist`,
+    // path: `${__dirname}/dist`,
+    path: path.join(__dirname, '/dist'),
     filename: 'main.js'
   }
 }
